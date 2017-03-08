6de539f5ef11665e473e4f94023c69aa700018f5
MelofunkCodes <myeoh27@gmail.com>
2017-03-08 13:45:22 -0500

    first commit. Added component called melinda

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Melinda from './melinda';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Melinda />
+      </div>
+    );
+  }
+}
+
+export default App;
